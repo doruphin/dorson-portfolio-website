7b0a35a6b347449fba61466141b6531bf433f723
dorson <apersonnameddorson@gmail.com>
2025-07-06 18:08:26 -0700

    imported model and looking code

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,109 @@
-import * as THREE from 'three'
-import { createRoot } from 'react-dom/client'
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import './styles.css'
+import * as THREE from "three";
+import { createRoot } from "react-dom/client";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import "./styles.css";
 
-function Box(props: JSX.IntrinsicElements['mesh']) {
-const meshRef = useRef<THREE.Mesh>(null!)
-const [hovered, setHover] = useState(false)
-const [active, setActive] = useState(false)
-useFrame((state, delta) => (meshRef.current.rotation.x += delta))
-return (
-  <mesh
-    {...props}
-    ref={meshRef}
-    scale={active ? 1.5 : 1}
-    onClick={(event) => setActive(!active)}
-    onPointerOver={(event) => setHover(true)}
-    onPointerOut={(event) => setHover(false)}>
-    <boxGeometry args={[1, 1, 1]} />
-    <meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} />
-  </mesh>
-)
+let neck: THREE.Object3D<THREE.Object3DEventMap>;
+let waist: THREE.Object3D<THREE.Object3DEventMap>;
+
+function Scene() {
+  const avatar = useLoader(GLTFLoader, "/static/models/avatar.glb");
+  avatar.scene.traverse((o) => {
+    console.log(o.name);
+    if (o.name === "Neck") {
+      neck = o;
+    }
+    if (o.name === "Body") {
+      waist = o;
+    }
+    if (o.name === "BicepR") {
+      o.rotation.z = -0.5;
+    }
+    if (o.name === "BicepL") {
+      o.rotation.z = -0.5;
+    }
+  });
+  return <primitive object={avatar.scene} scale={1} position={[0, -3, 0]} />;
 }
 
-createRoot(document.getElementById('root')).render(
-<Canvas>
-  <ambientLight intensity={Math.PI / 2} />
-  <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-  <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-  <Box position={[-1.2, 0, 0]} />
-  <Box position={[1.2, 0, 0]} />
-</Canvas>,
-)
+createRoot(document.getElementById("root")).render(
+  <Canvas className="h-128">
+    <ambientLight intensity={Math.PI / 2} />
+    <spotLight
+      position={[10, 10, 10]}
+      angle={0.15}
+      penumbra={1}
+      decay={0}
+      intensity={Math.PI}
+    />
+    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+    <Scene />
+  </Canvas>,
+);
+
+document.addEventListener("mousemove", function (e) {
+  var mousecoords = getMousePos(e);
+});
+
+function getMousePos(e) {
+  return { x: e.clientX, y: e.clientY };
+}
+
+document.addEventListener("mousemove", function (e) {
+  var mousecoords = getMousePos(e);
+  if (neck && waist) {
+    moveJoint(mousecoords, neck, 50);
+    moveJoint(mousecoords, waist, 30);
+  }
+});
+
+function moveJoint(mouse, joint, degreeLimit) {
+  let degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit);
+  joint.rotation.y = THREE.MathUtils.degToRad(degrees.x);
+  joint.rotation.x = THREE.MathUtils.degToRad(degrees.y);
+}
+
+function getMouseDegrees(x, y, degreeLimit) {
+  let dx = 0,
+    dy = 0,
+    xdiff,
+    xPercentage,
+    ydiff,
+    yPercentage;
+
+  let w = { x: window.innerWidth, y: window.innerHeight };
+
+  // Left (Rotates neck left between 0 and -degreeLimit)
+
+  // 1. If cursor is in the left half of screen
+  if (x <= w.x / 2) {
+    // 2. Get the difference between middle of screen and cursor position
+    xdiff = w.x / 2 - x;
+    // 3. Find the percentage of that difference (percentage toward edge of screen)
+    xPercentage = (xdiff / (w.x / 2)) * 100;
+    // 4. Convert that to a percentage of the maximum rotation we allow for the neck
+    dx = ((degreeLimit * xPercentage) / 100) * -1;
+  }
+  // Right (Rotates neck right between 0 and degreeLimit)
+  if (x >= w.x / 2) {
+    xdiff = x - w.x / 2;
+    xPercentage = (xdiff / (w.x / 2)) * 100;
+    dx = (degreeLimit * xPercentage) / 100;
+  }
+  // Up (Rotates neck up between 0 and -degreeLimit)
+  if (y <= w.y / 2) {
+    ydiff = w.y / 2 - y;
+    yPercentage = (ydiff / (w.y / 2)) * 100;
+    // Note that I cut degreeLimit in half when she looks up
+    dy = ((degreeLimit * 0.5 * yPercentage) / 100) * -1;
+  }
+
+  // Down (Rotates neck down between 0 and degreeLimit)
+  if (y >= w.y / 2) {
+    ydiff = y - w.y / 2;
+    yPercentage = (ydiff / (w.y / 2)) * 100;
+    dy = (degreeLimit * yPercentage) / 100;
+  }
+  return { x: dx, y: dy };
+}
