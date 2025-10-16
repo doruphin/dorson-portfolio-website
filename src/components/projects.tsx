b1496edfa7a943477f23de8b6ed91779e9ff1eb0
Dorson T. <apersonnameddorson@gmail.com>
2025-10-15 21:40:57 -0700

    inline styles

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -21,7 +21,7 @@ const projects: Project[] = [
   {
     link: "https://github.com/doruphin/fretnot",
     icon: "images/fretnot.ico",
-    bg: "bg-[url(images/fretnot_bg.png)]",
+    bg: "images/fretnot_bg.png",
     title: "FretNot",
     description:
       "FretNot was a project my team and I created for Stormhacks 2025, a 24 hour hackathon. It was an attachment on to your guitar that would use lasers to display the proper frets and strings of certain chords, tuned for Creep by Radiohead. We ended up winning the IEEE award for engineering, so I'm pretty proud of this one.",
@@ -30,7 +30,7 @@ const projects: Project[] = [
   {
     link: "https://github.com/TightGrapes/GMTK-2025",
     icon: "images/execution.ico",
-    bg: "bg-[url(images/execution_bg.png)]",
+    bg: "images/execution_bg.png",
     title: "Execution Order",
     description:
       "Execution Order was a project created for GMTK 2025, the largest game jam in Itch.io history. We created a puzzle game in under 72 hours, and I mainly worked on implementing the visuals and obstacles of the game. We placed in the top 5% amongst 37,000 participants, which is pretty impressive conidering there were full on game companies in the competition.",
@@ -39,7 +39,7 @@ const projects: Project[] = [
   {
     link: "https://github.com/doruphin/bublstore",
     icon: "images/bublstore.ico",
-    bg: "bg-[url(images/bublstore_bg.png)]",
+    bg: "images/bublstore_bg.png",
     title: "bublstore",
     description:
       "bublstore was the first personal project I created just to learn the basics of web development and more specifically, the backend element of it. It was created using React + Tailwind, and featured an AI chatbot using a local Ollama LLM. Please do not enter in your credit card information though, as I'm pretty sure I stored it in plain text :).",
@@ -48,7 +48,7 @@ const projects: Project[] = [
   {
     link: "https://github.com/TightGrapes/Pikspace",
     icon: "images/pikspace.ico",
-    bg: "bg-[url(images/pikspace_bg.png)]",
+    bg: "images/pikspace_bg.png",
     title: "Pikspace",
     description:
       "Pikspace was a project created for nwHacks 2025, the premiere hackathon at UBC. It was intended to be a fully fledged DSLR simulator in VR, mainly used as practice for exotic locations. I worked on the VR interactions with the camera adapting the various options such as ISO and aperture speed.",
@@ -57,7 +57,7 @@ const projects: Project[] = [
   {
     link: "https://github.com/Andrewyx/Flicker",
     icon: "images/flicker.ico",
-    bg: "bg-[url(images/flicker_bg.png)]",
+    bg: "images/flicker_bg.png",
     title: "Flicker",
     description:
       'Flicker was a game created for Dungeon Crawler Jam 2023, and was the first "coding" competition I created. I mainly worked on the art and implementation of the game. While the game isnt particularly "good" or even "playable", it holds a special place in my heart for being my entry point into my love of game development.',
@@ -105,10 +105,8 @@ export function Projects() {
       <div className="w-full h-full border-1 rounded-b-lg p-4 border-t-0 flex flex-col bg-secondary">
         <div className="w-full h-full ">
           <div
-            className={clsx(
-              "w-full h-full bg-cover rounded-lg border-1 bg-[url(images/fretnot_bg.png)]",
-              activeProject.bg,
-            )}
+            className={"w-full h-full bg-cover rounded-lg border-1"}
+            style={{ backgroundImage: `url(${activeProject.bg})` }}
           >
             <div className="h-full w-1/2 p-4">
               <div className="bg-quaterniary rounded-lg h-full p-4 border-1 flex flex-col">
@@ -133,8 +131,6 @@ export function Projects() {
             </div>
           </div>
         </div>
-
-        <img src="images/execution_bg.png" />
       </div>
     </div>
   );
